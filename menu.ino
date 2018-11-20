69a66abb724b4aa8f39194967d565ec4e02043ee
Jani Lehto <jani.lehto@ictpro.labra>
2018-11-20 11:48:32 +0200

    Linkki seuraavaan valikkoon toimii

--- menu.ino
+++ menu.ino
@@ -1,8 +1,9 @@
 //Malli funktio-osoittimesta
 typedef void (* menuLine)(int);
-typedef struct{
+struct menuElement{
   menuLine mL;
-}menuElement;
+  struct menuElement* t;
+};
 
 //Funktio joka piirtää yhden rivin valikkoon
 void menu1line1(int row){
@@ -24,15 +25,6 @@ void menu2line2(int row){
   Serial.print("Menu 2 Line 2 param: ");
   Serial.println(row);
 }
-//struct jossa tekstin luonti ja linkki seuraavaan valikkoon
-menuElement mEmenu1line1{
-  &menu1line1
-  //Linkki puuttuu
-};
-menuElement mEmenu1line2{
-  &menu1line2
-  //Linkki puuttuu
-};
 menuElement mEmenu2line1{
   &menu2line1
   //Linkki puuttuu
@@ -41,15 +33,26 @@ menuElement mEmenu2line2{
   &menu2line2
   //Linkki puuttuu
 };
+menuElement Menu21[]={
+  mEmenu2line1,
+  mEmenu2line2
+};
+//struct jossa tekstin luonti ja linkki seuraavaan valikkoon
+menuElement mEmenu1line1{
+  &menu1line1,
+  Menu21
+};
+menuElement mEmenu1line2{
+  &menu1line2
+  //Linkki puuttuu
+};
+
 //Valikko Menu1
 menuElement Menu1[]={
   mEmenu1line1,
   mEmenu1line2
 };
-menuElement Menu21[]={
-  mEmenu2line1,
-  mEmenu2line2
-};
+
 void setup(){
   Serial.begin(9600);
 }
@@ -62,7 +65,7 @@ void loop(){
     switch(x){
       case 'a':menuRow++;break;
       case 'd':menuRow--;break;
-      case 's':menu=Menu21;
+      case 's':menu=menu[menuRow].t;
     }
   }
   menu[0].mL(menuRow);
